bc807b99655fc42fbeff2ab0680f051784c390c8
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-04-02 16:48:04 +0200

    Init  Solorush2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section className="AppComponent">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <p>App Component</p>
+    </section>
+  );
+}
+
+export default App;
