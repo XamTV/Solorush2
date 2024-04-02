0ffee1c60733adf58a1dbc97219991b2c25c73a5
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-04-02 19:38:06 +0200

    Made some changes on NavBar and About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,37 @@ import ProfilePic from "../assets/ProfilePic.jpg";
 
 function About() {
   return (
-    <section className="flex flex-col bg-red-500 h-screen">
+    <section className="flex flex-col  h-screen ">
       <img
-        className="w-[300px] self-center rounded-full"
+        className="w-[300px] self-center rounded-full mt-5"
         src={ProfilePic}
         alt="ZZZ"
       />
-      <h1 className="text-center max-lg:text-5xl max-lg:px-8 text-animation">
-        Hello
-      </h1>
+      <h1 className="text-3xl m-5">Hello, I&apos;m Maxime !</h1>
+      <h2 className="text-xl m-5">
+        web & mobile web developer based in France{" "}
+      </h2>
+      <p className="mx-[25%] m-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sequi
+        nulla aliquam maxime omnis tenetur hic cum? Error eligendi quasi
+        reprehenderit vero, laborum esse blanditiis officia sit, delectus unde
+        quaerat quam nemo culpa, vitae maiores quae laudantium placeat expedita
+        consequatur excepturi? Iure, ullam totam dolore facere maiores cum quas
+        perspiciatis quod aliquid est atque quaerat sequi quis illo tempore
+        laboriosam praesentium? Excepturi doloribus eos, beatae dolorem
+        architecto magnam ut veritatis similique iure rem et maiores. Fuga,
+        aperiam nesciunt? Totam esse provident, consectetur veniam doloremque
+        dolorum neque alias facere nemo incidunt iste magnam culpa illum iusto
+        veritatis? Cum sequi tempore aliquam.
+      </p>
+      <div className="flex-row ">
+        <a className="bg-blue-500 p-2" href="#Contact">
+          Contact
+        </a>{" "}
+        <a className="bg-blue-500 p-2" href="#Projects">
+          My Projects
+        </a>
+      </div>
     </section>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,21 @@
 function Nav() {
   return (
     <nav className="bg-[#001b2e] sticky top-0 flex justify-between mx-[25%] my-5 px-20 py-2 rounded-3xl text-lg font-bold">
-      <a href="#Home">Home</a>
-      <a href="#Skills">Skills</a>
-      <a href="#About">About</a>
-      <a href="#Projects">Projects</a>
-      <a href="#Contact">Contact</a>
+      <a className="hover:text-yellow-500" href="#Home">
+        Home
+      </a>
+      <a className="hover:text-yellow-500" href="#Skills">
+        Skills
+      </a>
+      <a className="hover:text-yellow-500" href="#About">
+        About
+      </a>
+      <a className="hover:text-yellow-500" href="#Projects">
+        Projects
+      </a>
+      <a className="hover:text-yellow-500" href="#Contact">
+        Contact
+      </a>
     </nav>
   );
 }
