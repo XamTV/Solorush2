1c3384323abd278b6fad8c420f694f599e2395e7
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-04-03 13:08:28 +0200

    Fixed some things and added Dark Mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Skills from "./components/Skills";
+import { useState } from "react";
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+
   return (
     <>
-      <Nav />
+      <Nav isDarkTheme={isDarkTheme} setIsDarkTheme={setIsDarkTheme} />
       <About />
       <Skills />
       <Projects />
