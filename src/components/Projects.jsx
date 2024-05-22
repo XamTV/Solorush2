f27f7926aea48711301ced2e6259c2ce43a261aa
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-05-22 20:50:45 +0200

    Reworking portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,32 +5,34 @@ import "../styles/Projects.css";
 function Projects() {
   return (
     <>
-      <h2 id="Projects">My Projects</h2>
-      <section className="projectcard-component">
-        {myProjects.map((project) => (
-          <article className="projectcard-article" key={project.id}>
-            <figure>
-              <img
-                className="project-image"
-                src={project.image}
-                alt="project"
-              />
-            </figure>
-            <figcaption className="projectcard-caption">
-              <h2>{project.name}</h2>
-              <p>{project.desc}</p>
-              <div>
-                <a href={project.github} target="_blank">
-                  Repo Github
-                </a>
-                <a href={project.link} target="_blank">
-                  Démo
-                </a>
-              </div>
-            </figcaption>
-          </article>
-        ))}
-      </section>
+      <div className="projetcard-container">
+        <h2>My Projects</h2>
+        <section className="projectcard-component">
+          {myProjects.map((project) => (
+            <article className="projectcard-article" key={project.id}>
+              <figure>
+                <img
+                  className="project-image"
+                  src={project.image}
+                  alt="project"
+                />
+              </figure>
+              <figcaption className="projectcard-caption">
+                <h2>{project.name}</h2>
+                <p>{project.desc}</p>
+                <div>
+                  <a href={project.github} target="_blank">
+                    Repo Github
+                  </a>
+                  <a href={project.link} target="_blank">
+                    Démo
+                  </a>
+                </div>
+              </figcaption>
+            </article>
+          ))}
+        </section>
+      </div>
       <div id="Contact"></div>
     </>
   );
