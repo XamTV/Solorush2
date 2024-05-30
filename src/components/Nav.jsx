ec1be40e353dfbea352f40e7f5b09bafc6c2796f
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-05-31 00:13:08 +0200

    Adding some css , fixing fonts

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,24 @@
 import PropTypes from "prop-types";
 
-function Nav({ isDarkTheme, setIsDarkTheme }) {
+function Nav({ isDarkTheme }) {
   return (
     <nav
       className={` bg-[#001b2e] scroll-smooth sticky top-0 z-10 flex justify-between mx-[25%] my-5 px-20 py-2 rounded-3xl text-lg font-bold 
     ${isDarkTheme ? "bg-[#001b2e]" : "border-black bg-[#adb6c4] text-black"}`}
     >
       <a className="hover:text-yellow-500 pt-2" href="#Home">
-        Home
+        A Propos
       </a>
       <a className="hover:text-yellow-500 pt-2" href="#Skills">
         Skills
       </a>
 
       <a className="hover:text-yellow-500 pt-2" href="#Projects">
-        Projects
+        Projets
       </a>
       <a className="hover:text-yellow-500 pt-2" href="#Contact">
-        Socials
+        Mes Réseaux
       </a>
-      <input
-        type="checkbox"
-        checked={isDarkTheme ? "checked" : ""}
-        onClick={() => setIsDarkTheme(!isDarkTheme)}
-      />
     </nav>
   );
 }
